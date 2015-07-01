ee88993f28f797bb05d1a7c0a07dad5d666d02f5
U-PS14B22\vantan <akira206@gmail.com>
2015-07-01 18:34:23 +0900

    回転速度制御を追加

--- src/Missile/missile.cpp
+++ src/Missile/missile.cpp
@@ -1,13 +1,14 @@
 
 #include "missile.h"
 #include "../MyLib/key.h"
+#include <cmath> // sqrt
 
 
 Missile::Missile(CameraPersp& cam) :
 pos(0, -100, 0),
 size(10, 20, 10),
 rotate(0, 0, 0),
-rot_speed(0, 0, 10),
+rot_speed(0, 0, 6),
 velocity(0, 10, 0),
 color(1, 1, 1),
 cam(cam) {}
@@ -30,9 +31,16 @@ void Missile::horming() {
   Vec3f Aim = target - pos;
 
   // 進行方向に対してターゲットが右にいるか左にいるかを判定
-  if ((Forward.x * Aim.y - Forward.y * Aim.x) > 0) {
+  /*if ((Forward.x * Aim.y - Forward.y * Aim.x) > 0) {
     rotate += rot_speed;
-  } else { rotate -= rot_speed; }
+  } else { rotate -= rot_speed; }*/
+
+  // ターゲットに対して追尾する際の回転速度を調整する処理
+  // ターゲットが進行方向||後ろにいると回転速度減少
+  float lengthAim = std::sqrt(Aim.x * Aim.x + Aim.y * Aim.y);
+  Aim.x /= lengthAim; Aim.y /= lengthAim;
+  float Cross = Forward.x * Aim.y - Forward.y * Aim.x;
+  rotate.z += rot_speed.z * Cross / velocity.y; // 移動速度で割ってあげるといい塩梅になる
 }
 
 void Missile::update() {
