5b353d13a24016d07ba6533b4bfbd540af17ca43
U-PS14B22\vantan <akira206@gmail.com>
2015-07-05 22:37:39 +0900

    指定した軸に対して回転,回転後の行列から取り出した座標がおかしい

--- src/Missile/missile.cpp
+++ src/Missile/missile.cpp
@@ -5,42 +5,11 @@
 
 
 Missile::Missile(CameraPersp& cam) :
-pos(0, -100, 0),
-size(10, 20, 10),
-rotate(0, 0, 0),
-rot_speed(0, 0, 6),
-velocity(0, 1, 0),
-color(1, 1, 1),
-cam(cam) {}
+cam(cam),
+velocity(0, 0, 0) {}
 
 
 void Missile::horming() {
-  gl::pushModelView();
-
-  gl::translate(pos);
-  gl::rotate(rotate);
-  gl::translate(velocity);
-  auto current_matrix = gl::getModelView() * cam.getInverseModelViewMatrix();
-
-  gl::popModelView();
-
-  Vec3f Velocity = Vec3f(current_matrix.m03,
-                         current_matrix.m13,
-                         current_matrix.m23);
-  Vec3f Forward = Velocity - pos;
-  Vec3f Aim = target - pos;
-
-  console() << "Forward = " << Forward.normalized() << std::endl;
-  console() << "Aim = " << Aim.normalized() << std::endl;
-
-  float dot = Forward.normalized().dot(Aim.normalized());
-  console() << "dot = " << dot << std::endl;
-
-  if (dot < 1.0f) dot = 1.0f;
-
-  float angle = std::acosf(dot); // 微動だにせず0
-  console() << "angle" << angle << std::endl;
-
 
 }
 
@@ -49,22 +18,27 @@ void Missile::update() {
 }
 
 void Missile::draw() {
-  gl::color(color);
-
   gl::pushModelView();
 
-  gl::translate(pos);
+  static float angle = 0.f;
+
+  if (Key::get().isPush(KeyEvent::KEY_d)) angle += 0.1f;
+  if (Key::get().isPush(KeyEvent::KEY_a)) angle -= 0.1f;
+  if (Key::get().isPress(KeyEvent::KEY_w)) velocity.y += 2;
+  if (Key::get().isPress(KeyEvent::KEY_s)) velocity.y -= 2;
+
+  Vec3f axis(1, 0, 0);
+  Matrix44f rotate = Matrix44f::createRotation(axis, angle);
+  rotate.setTranslate(Vec3f::zero());
+  gl::translate(last_pos);
   gl::rotate(rotate);
   gl::translate(velocity);
-  gl::drawCube(Vec3f::zero(), size);
+  gl::drawStrokedCube(Vec3f::zero(), Vec3f(10, 10, 10));
 
-  // ビュー行列にカメラの逆行列を掛けてワールド行列を取り出す
-  auto current_matrix = gl::getModelView() * cam.getInverseModelViewMatrix();
+  auto matrix = gl::getModelView() * cam.getInverseModelViewMatrix();
+  //last_pos = Vec3f(matrix.m03, matrix.m13, matrix.m23);
 
-  // m03 == x, m13 == y, m23 == z
-  pos = Vec3f(current_matrix.m03,
-              current_matrix.m13,
-              current_matrix.m23);
+  console() << "[x = " << matrix.m03 << "][y = " << matrix.m13 << "][z = " << matrix.m23 << "]" << std::endl;
 
   gl::popModelView();
 }
